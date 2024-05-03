e41a4ab074dfea4aa89c34397925fd00e0f256af
pavanreddy-tw <137087176+pavanreddy-tw@users.noreply.github.com>
2024-05-03 00:01:57 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-# !/bin/bash
-
 sudo apt update
 sudo apt install -y python3-pip
 
@@ -9,4 +7,4 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
+streamlit run server.py --server.port 8501 --server.address 0.0.0.0
